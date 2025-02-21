f680eaf4fc51cca0e3c27254b098267312a5a2b7
jchopra05 <jchopra@g.hmc.edu>
2025-02-21 15:58:58 -0800

    first commit

---
+++ services/web/project/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    APP_FOLDER = os.getenv("APP_FOLDER", basedir)
+    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
+    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
